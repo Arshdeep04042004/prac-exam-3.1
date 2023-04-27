61cba38e74c547575baf86eb62b41d942da51d2f
Arshdeep04042004 <111043104+Arshdeep04042004@users.noreply.github.com>
2023-04-27 12:54:40 +0930

    Add files via upload

---
+++ 1.1.cpp
@@ -0,0 +1,7 @@
+Car();
+Car(int price);   // creates a Car with a price
+
+int price;
+int emissions;   // CO2 Emissions
+
+void drive(int kms);
---
+++ 2.1.cpp
@@ -0,0 +1,8 @@
+Tesla();            
+Tesla(char model, int price);  
+  
+char model;
+float batteryPercentage;      // Initially 100%
+
+void chargeBattery(int mins);            
+void drive(int kms); 
